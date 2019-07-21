aa49bf2eac8f9a2a0d96029091c415cfc62d976c
Muhammad Dawood <mdawoodch16@gmail.com>
2019-07-21 09:29:50 +0500

    first commit

---
+++ app/Http/Controllers/PollsController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Poll;
+use Illuminate\Http\Request;
+use App\Http\Resources\Poll as PollResponse;
+use Illuminate\Support\Facades\Validator;
+
+class PollsController extends Controller
+{
+    public function index(){
+        return response()->json(Poll::all(),200);
+    }
+
+    public function show($id){
+        $poll = Poll::find($id);
+        if (is_null($poll))
+            return response()->json(null,404);
+        $response = new PollResponse(Poll::findOrFail($id));
+        return response()->json($response,200);
+    }
+
+    public function store(Request $request){
+        $rules = [
+            'title' => 'required|max:10',
+        ];
+        $validator = Validator::make($request->all(),$rules);
+        if($validator->fails()){
+            return response()->json($validator->errors(),400);
+        }
+        $poll = Poll::create($request->all());
+        return response()->json($poll, 201);
+    }
+    public function update(Request $request , Poll $poll){
+        $poll->update($request->all());
+        return response()->json($poll,200);
+    }
+    public function delete(Poll $poll){
+        $poll->delete();
+        return response()->json(null,204);
+    }
+    public function errors(){
+        return response()->json([
+            'msg' => 'Message is required',
+        ],501);
+    }
+}
---
+++ app/Http/Resources/Poll.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Poll extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return mb_strimwidth($this->title,0,9,'....');
+    }
+}
